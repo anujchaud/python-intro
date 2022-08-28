a0a637beb7f942cbd457919b642f41f3afb926b0
anujchaud <101237792+anujchaud@users.noreply.github.com>
2022-08-28 22:15:30 +0530

    Add files via upload

---
+++ boolen.py
@@ -0,0 +1,22 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+a=input("enter first number")
+b=input("input second number")
+if b>a:
+    print("a greater than b")
+else:
+    print("b is greater than a")  
