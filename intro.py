c4e59b4a7ff687e64dcd86e73fe428fc8d681b66
anujchaud <101237792+anujchaud@users.noreply.github.com>
2022-08-28 22:14:15 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,8 @@
+#python is a high level interprite programming language
+# it is used for
+# 1 web deplopment(server side)
+#2 software deplopment
+#3 mathmatic
+#4 system scripiting
+a="hello"
+print(a)
